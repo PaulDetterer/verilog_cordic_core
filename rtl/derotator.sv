8fc166d5f4f008b8ab5f69cfbb0f7aa792903c2a
Paul Detterer <paul@Pauls-iMac.local>
2020-11-17 18:09:28 +0100

    Changes to be committed: 	modified:   ../../nb/CRotateF.ipynb 	new file:   ../../nb/FDC_HD.ipynb 	new file:   ../../rtl/derotator.sv 	modified:   ../../rtl/fdc.sv 	new file:   ../stimuli/iq_if_clean.txt 	new file:   tb_fdc.sv

---
+++ rtl/derotator.sv
@@ -0,0 +1,51 @@
+/*  file:        derotator.sv
+    author:      Paul Detterer
+    release:     17/11/2020
+
+    First Quadrant Cordic based Derotator 
+
+    Intended to be used for frequency down conversion in digital baseband
+
+    The design is inspired by the CORDIC implementation of Dale Drinkard from OpenCores
+
+*/
+
+module derotator 
+#(
+  parameter integer iteration = 0,
+  parameter BW=12,
+  parameter ABW=12,
+  parameter signed [ABW-1:0] tangle = 0,
+)
+(
+  input wire clk,
+  input wire rst,
+  input wire signed  [BW-1:0]    x_i,
+  input wire signed  [BW-1:0]    y_i,
+  input wire signed  [BW-1:0] z_i,
+  output wire signed [BW-1:0]    x_o,
+  output wire signed [BW-1:0]    y_o,
+  output wire signed [ABW-1:0] z_o
+  );
+  
+  reg signed [BW-1:0] x_1;
+  reg signed [BW-1:0] y_1;
+  reg signed [ABW-1:0] z_1;
+  wire signed [BW-1:0] x_i_shifted = x_i >>> iteration;
+  wire signed [BW-1:0] y_i_shifted = y_i >>> iteration;
+
+  always_comb
+      if (z_i < 0) begin // Rotate Mode
+        x_1 <= x_i - y_i_shifted; //shifter(y_1,i); //(y_1 >> i);
+        y_1 <= y_i + x_i_shifted; //shifter(x_1,i); //(x_1 >> i);
+        z_1 <= z_i + tangle;
+      end else begin
+        x_1 <= x_i + y_i_shifted; //shifter(y_1,i); //(y_1 >> i);
+        y_1 <= y_i - x_i_shifted; //shifter(x_1,i); //(x_1 >> i);
+        z_1 <= z_i - tangle;
+      end
+    end
+  assign x_o = x_1;
+  assign y_o = y_1;
+  assign z_o = z_1;
+endmodule
--- rtl/fdc.sv
+++ rtl/fdc.sv
@@ -1,135 +1,171 @@
-// This is an implementation of frequency down converter (fdc)
-// Using cordic accumulator and fir filter
 
-`define COMBINATORIAL
-`include "cordic.v"
+// Angle representation is always a 0.bbbbbb of a circle
+// 100000 -> half circle (pi)
+// 111111 -> 2*pi (almost)
 
-module fdc
+
+module FDC
 #(
-    parameter BW=12,
-    parameter ABW=16,
-
+    parameter BW = 12,
+    parameter ABW = 10,
 )
 (
-    input clk,
-    input rstb,
-    input [BW-1:0] Iin, 
-    input [BW-1:0] Qin, 
-    input [BW-1:0] A,
-    output [BW-1:0] Iout, 
-    output [BW-1:0] Qout 
-);
-    reg [BW-1:0] IinR,QinR;
-    reg [ABW-1:0] Phase;
-    reg             clk2,clk4;
-    wire [ABW-1:0] PhaseOut;
+    input clk_fs,  //  Sampling clock
+    input clk_fso2,  // Decimation clock fsample/2
+    input clk_fso4,  // Decimation clock fsample/4
+    input rstb, // Reset
 
-    wire [BW-1:0] Irot, Qrot;
-    wire [BW-1:0] Idec1, Qdec1;
-    wire [BW-1:0] Idec2, Qdec2;
-    wire [BW-1:0] If1, Qf1;
-    wire [BW-1:0] If2, Qf2;
+    input [BW-1:0] Iin,
+    input [BW-1:0] Qin,
+    input [ABW-1:0] Wif,
+    output [BW-1:0] Iout,
+    output [BW-1:0] Qout
+)
+    reg  [BW-1:0] Iin_s, Qin_s;
+    wire  [BW-1:0] Igr, Qgr;
 
-    //Latch Inputs
-    always_ff @(posedge clk or negedge rstb)
+    // Sample
+    always_ff @(posedge clk_fs or negedge rstb)
         if (rstb == 0)
          begin
-            IinR <= 'd0;
-            QinR <= 'd0;
-         end   
+            Iin_s <= 'd0;
+            Qin_s <= 'd0;
+         end
         else
          begin
-            IinR <= Iin;
-            QinR <= Qin;
+            Iin_s <= Iin;
+            Qin_s <= Qin;
          end
 
-    // Divide Clocks
-    always_ff @(posedge clk or negedge rstb)
+    
+    // Compute rotation angle theta 
+    reg [ABW-1:0] theta; 
+
+    always_ff @(posedge clk_fs or negedge rstb)
         if (rstb==0)
-            clk2 <= 0;
+            theta <= 0;
         else
-            clk2 <= ~clk2;
+            theta <= unsigned(theta) + unsigned(Wif);
 
-    alsways_ff @(posedge clk2 or negedge rstb)
-        if (rstb==0)
-            clk4 <= 0;
-        else
-            clk4 <= clk4;
+    // Gross Rotation
+    always_comb
+        case(theta[ABW-1:ABW-2])
+            2'b00    :  {Igr,Qgr} = {Iin_s,Qin_s}; // rot 0
+            2'b01    :  {Igr,Qgr} = {Qin_s,-signed(Iin_s)}; //rot p/2
+            2'b10    :  {Igr,Qgr} = {-signed(Iin_s),-signed(Qin_s)}; //rot p
+            2'b11    :  {Igr,Qgr} = {-signed(Qin_s),Iin_s}; // rot 3pi/2
+        endcase
 
-    //Calculate Phase
-    always_ff @(posedge clk or negedge rstb)
-        if (rstb == 0)
-            Phase <= 'd0;
-        else
-            Phase <= $signed(Phase) + $signed(A);
+    // Fine Rotation
+    // Define the angle function
+    function [ABW-1:0] tangle;
+        input [3:0] i;
+        begin
+            case(i)
+			 3'd0 : tangle = 10'd128;
+			 3'd1 : tangle = 10'd76;
+			 3'd2 : tangle = 10'd40;
+			 3'd3 : tangle = 10'd20;
+			 3'd4 : tangle = 10'd10;
+			 3'd5 : tangle = 10'd5;
+			 3'd6 : tangle = 10'd3;
+			 3'd7 : tangle = 10'd1;
 
-    //Gross Derotation
-    TODO
+            endcase
+        end
+    endfunction
 
-    //Fine Derotation
-    TODO
+    wire signed [BW-1:0] x [8-1:0];
+    wire signed [BW-1:0] y [8-1:0];
+    wire signed [ABW-1:0] z [8-1:0];
 
-    //Filter 1
-    fir #(.BW(12),.N(5)) uFIRI1 (
-        .CK(clk),
+    assign x[0] = Igr;
+    assign y[0] = Qgr;
+    assign z[0] = theta;
+    wire Ifrt = x[8-1];
+    wire Qfrt = y[8-1];
+    wire ThetaR = z[8-1];
+
+    genvar i;
+    generate for(i=0;i<8-1;i=i+1) begin
+      derotator uDerotator (clk_fs,!rstb,x[i],y[i],z[i],x[i+1],y[i+1],z[i+1]);
+      defparam uDerotator.BW = BW;
+      defparam UDerotator.ABW = ABW;
+      defparam uDerotator.iteration = i;
+      defparam uDerotator.tangle = tanangle(i);
+    end 
+    endgenerate
+
+    
+    // Filter 1
+    wire [BW-1:0] Ilpf1,Qlpf1;
+    reg  [BW-1:0] Idec1,Qdec1;
+
+    fir #(.BW(BW),.N(5),.S(BW-1)) uIFIR1 (
+        .CK(clk_fs),
         .RB(rstb),
-        .X(I_rot),
+        .X(Ifrt),
         .C({12'd211,12'd420,12'd503,12'd420,12'd211}),
-        .Y(I_f1));
-    fir #(.BW(12),.N(5)) uFIRQ1 (
-        .CK(clk),
+        .Y(Ilpf1));
+    
+    fir #(.BW(BW),.N(5),.S(BW-1)) uQFIR1 (
+        .CK(clk_fs),
         .RB(rstb),
-        .X(Q_rot),
+        .X(Qfrt),
         .C({12'd211,12'd420,12'd503,12'd420,12'd211}),
-        .Y(Q_f1));
+        .Y(Qlpf1));
 
     //Decimate 1
 
-    always_ff @(posedge clk2 or negedge rstb)
+    always_ff @(posedge clk_fso2 or negedge rstb)
         if (rstb==0)
          begin
-            I_dec1 <= 'd0;
-            Q_dec1 <= 'd0;
+            Idec1 <= 'd0;
+            Qdec1 <= 'd0;
          end
         else
          begin
-            I_dec1 <= I_f1;
-            Q_dec1 <= Q_f1;
+            Idec1 <= Ilpf1;
+            Qdec1 <= Qlpf1;
          end
 
-    // Filter 2
+    //Filter 2
 
-    fir #(.BW(12),.N(5)) uFIRI2 (
-        .CK(clk),
-        .RB(rstb),
-        .X(I_dec1),
-        .C({12'd211,12'd420,12'd503,12'd420,12'd211}),
-        .Y(I_f2));
-    fir #(.BW(12),.N(5)) uFIRI2 (
-        .CK(clk),
-        .RB(rstb),
-        .X(Q_dec1),
-        .C({12'd211,12'd420,12'd503,12'd420,12'd211}),
-        .Y(Q_f2));
-
-    // Decimate 2
-    //
-    always_ff @(posedge clk4 or negedge rstb)
-        if (rstb==0)
-         begin
-            I_dec2 <= 'd0;
-            Q_dec2 <= 'd0;
-         end
-        else
-         begin
-            I_dec2 <= I_f2;
-            Q_dec2 <= Q_f2;
-         end
     
-    // Connect to outputs
+    wire [BW-1:0] Ilpf2,Qlpf2;
+    reg  [BW-1:0] Idec2,Qdec2;
 
-    assign Qout = Q_dec2;
-    assign Iout = I_dec2;
+    fir #(.BW(BW),.N(5),.S(BW-1)) uIFIR2 (
+        .CK(clk_fso2),
+        .RB(rstb),
+        .X(Idec1),
+        .C({12'd211,12'd420,12'd503,12'd420,12'd211}),
+        .Y(Illpf2));
+    
+    fir #(.BW(BW),.N(5),.S(BW-1)) uQFIR2 (
+        .CK(clk_fs),
+        .RB(rstb),
+        .X(Qdec1),
+        .C({12'd211,12'd420,12'd503,12'd420,12'd211}),
+        .Y(Qlpf2));
+    
+    //Decimate 2
 
 
-endmodule
+    always_ff @(posedge clk_fso4 or negedge rstb)
+        if (rstb==0)
+         begin
+            Idec2 <= 'd0;
+            Qdec2 <= 'd0;
+         end
+        else
+         begin
+            Idec2 <= Ilpf2;
+            Qdec2 <= Qlpf2;
+         end
+
+    assign Qout = Qdec2;
+    assign Iout = Idec2;
+
+endmodule    
+
